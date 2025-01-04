5a29f5207b9b9fd1cd6348306b8edab76c0e9bb5
mustafamuhamedahmed <133133697+mustafamuhamedahmed@users.noreply.github.com>
2025-01-04 19:43:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
-import ProductDetails from "./pages/ProductDetails";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
@@ -43,7 +43,7 @@ const App = () => {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
-                <Route path="/product/:id" element={<ProductDetails />} />
+                <Route path="/products/:productId" element={<ProductDetailsPage />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/login" element={<Login />} />
